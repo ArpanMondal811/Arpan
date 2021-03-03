a275da01ddf70c3a539ac8068c9b2c4c775131f4
ArpanMondal811 <79977602+ArpanMondal811@users.noreply.github.com>
2021-03-03 17:07:54 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60367c2e1a979b001ff31cc7",
+				"60367c2e1a979b001ff31cc9"
+			]
+		},
+		"60367c2e1a979b001ff31cc7": {
+			"id": "60367c2e1a979b001ff31cc7",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60367c2e1a979b001ff31cc9": {
+			"id": "60367c2e1a979b001ff31cc9",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
